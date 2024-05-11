a53d854feb10f0998f6b49a3f0be9c01c8b4e0d4
xray-robot <robot@xray.app>
2024-05-11 20:59:19 +0300

    Export schema types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import createClient, { HeadersOptions } from "openapi-fetch"
 import type { paths } from "./schema/schema"
+import * as PricingTypes from "./schema/schema"
 
 export default (baseUrl: string, headers?: HeadersOptions) => {
   return createClient<paths>({
